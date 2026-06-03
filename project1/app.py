ca1480614ba97234d1c9b069f696fa423d40121c
mohammad <mohamadhosein20000@gmail.com>
2026-06-03 10:47:53 +0400

    refactor

--- project1/app.py
+++ project1/app.py
@@ -1,23 +1,17 @@
-
-
-from langchain_huggingface import HuggingFaceEmbeddings
-
-import os
 import shutil
-from dotenv import load_dotenv
-load_dotenv()
 
 import pypdf
-from langchain_core.documents import Document
-from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
 from langchain_chroma import Chroma
-from langchain_ollama import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+load_dotenv()
 
 # --- 1. Load & split ---
 reader = pypdf.PdfReader("./project1/docs/data.pdf")
@@ -25,25 +19,19 @@ documents = [
     Document(page_content=page.extract_text(), metadata={"source": "./project1/docs/data.pdf", "page": i})
     for i, page in enumerate(reader.pages)
 ]
-
-splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-chunks = splitter.split_documents(documents)
-
+chunks = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50).split_documents(documents)
 
 # --- 2. Embed & store ---
-
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
 vectorstore = Chroma.from_documents(chunks, embeddings, persist_directory="./chroma_db", collection_name="data")
-
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
 
 # --- 3. LLM ---
 llm = ChatGroq(model="llama-3.1-8b-instant")
 
-# # --- 4. RAG prompt ---
+# --- 4. Prompt ---
 prompt = ChatPromptTemplate.from_template("""
-Answer the question using ONLY the context below. 
+Answer the question using ONLY the context below.
 If you don't know, say "I don't have enough information."
 
 Context:
@@ -53,8 +41,7 @@ Question: {question}
 """)
 
 def format_docs(docs):
-    context =  "\n\n".join(doc.page_content for doc in docs)
-    return context
+    return "\n\n".join(doc.page_content for doc in docs)
 
 # --- 5. Chain ---
 rag_chain = (
